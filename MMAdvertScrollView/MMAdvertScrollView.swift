965ee1fb6d117e3051096ef963bf5fde2f7d5ecc
yuhui <yuhui@123>
2021-01-27 18:29:13 +0800

    更改为public

--- MMAdvertScrollView/MMAdvertScrollView.swift
+++ MMAdvertScrollView/MMAdvertScrollView.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-@objc protocol MMAdvertScrollViewDelegate: AnyObject{
+@objc public protocol MMAdvertScrollViewDelegate: AnyObject{
     
     @objc optional func customCycleScrollViewCellClass(_ cycleView: MMAdvertScrollView) -> AnyClass
     @objc optional func customCycleScrollViewNibCellClass(_ cycleView: MMAdvertScrollView) -> AnyClass
@@ -14,7 +14,7 @@ protocol MMAdvertScrollViewValueDelegate {
     var title: String { get }
 }
 
-class MMAdvertScrollView: UIView {
+public class MMAdvertScrollView: UIView {
 
     var timeInterval: TimeInterval = 3.0
     var scrollDirection: UICollectionView.ScrollDirection = .vertical { didSet { layout.scrollDirection = scrollDirection } }
@@ -47,7 +47,7 @@ class MMAdvertScrollView: UIView {
         initialization()
     }
     
-    override func layoutSubviews() {
+    public override func layoutSubviews() {
         layout.itemSize = CGSize(width: frame.size.width, height: frame.size.height)
         if dataArray?.count ?? 0 > 1 { collectionView?.scrollToItem(at: IndexPath(item: 0, section: maxSectionCount >> 2), at: (scrollDirection == .vertical ? .bottom : .right), animated: false) }
     }
@@ -110,15 +110,15 @@ class MMAdvertScrollView: UIView {
 
 extension MMAdvertScrollView : UICollectionViewDelegate, UICollectionViewDataSource {
     
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
         return maxSectionCount
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return dataArray?.count ?? 0
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ID, for: indexPath)
     
         if delegate?.customCycleScrollViewNibCellClass?(self) != nil  && delegate?.customCycleScrollViewSet(cell, numberOfItems: indexPath.item, cycleView: self) != nil { return cell }
@@ -129,7 +129,7 @@ extension MMAdvertScrollView : UICollectionViewDelegate, UICollectionViewDataSou
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("didSelectItemAt --- indexPath\(indexPath.item)")
         delegate?.collectionView?(self, didSelectItemAt: indexPath.item)
     }
